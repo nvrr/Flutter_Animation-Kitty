df240ca52da5ec8faafba88b54f05ea42dbd02a3
nvrr <sureshvarma809@gmail.com>
2020-10-11 17:13:38 +0530

    Adding Animated Builder: wired-up an AnimatedBulder to buildLeftFlap

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -11,7 +11,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   // Cat Animation
   Animation<double> catAnimation;
   AnimationController catController;
-// Box Animation
+  // Box Animation
   Animation<double> boxAnimation;
   AnimationController boxController;
 
@@ -105,15 +105,21 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
 
   Widget buildLeftFlap() {
     return Positioned(
-       left: 3.0, top: 1.0,
-          child: Transform.rotate(
-          angle: pi * 0.6,
-          alignment: Alignment.topLeft,
-          child: Container(
-            height: 10.0,    
-            width: 125.0,
-            color: Colors.brown
-        ),
+      left: 3.0, top: 1.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10.0,    
+          width: 125.0,
+          color: Colors.brown
+          ),
+        builder: (context, child) {
+          return Transform.rotate(
+            child: child,
+            alignment: Alignment.topLeft,
+            angle: boxAnimation.value
+        );
+        }
       ),
     );
   }
