732dbce000dfd7e587832228d57667664f36dbdd
nvrr <sureshvarma809@gmail.com>
2020-10-11 13:41:39 +0530

    Changing Rotation point using Alignment.topLeft

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -89,6 +89,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   Widget buildLeftFlap() {
     return Transform.rotate(
         angle: pi/2.0,
+        alignment: Alignment.topLeft,
         child: Container(
           height: 10.0,
           width: 125.0,
