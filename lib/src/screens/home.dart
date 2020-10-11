0892d67d3bdcf44ca1399090e2883105324df4b9
nvrr <sureshvarma809@gmail.com>
2020-10-11 12:37:37 +0530

    Adjusting Tween Ranges

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -15,11 +15,11 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     super.initState();
 
     catController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 200),
       vsync: this,
     );
 
-    catAnimation = Tween(begin:-50.0, end: -80.0)
+    catAnimation = Tween(begin:-35.0, end: -80.0)
        .animate(
          CurvedAnimation(
            parent: catController,
@@ -51,8 +51,8 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
           child: Stack(
             overflow: Overflow.visible,
             children: [
-              buildBox(),
               buildCatAnimation(),
+              buildBox(),
             ],
           ),
         ),
